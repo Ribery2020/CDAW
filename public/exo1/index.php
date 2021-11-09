93620b3eab3f32d485b13b15861efca5c0b22718
Ribery <jiaqigao2020@gmail.com>
2021-11-09 11:41:39 +0100

    first

---
+++ public/exo1/index.php
@@ -0,0 +1,7 @@
+<?php
+echo "aujourd'hui " . date("Y/m/d") . "<br>";
+echo "aujourd'hui " . date("Y.m.d") . "<br>";
+echo "aujourd'hui " . date("Y-m-d") . "<br>";
+echo "aujourd'hui " . date("l");
+echo "time " . date("h:i:sa");
+?>
